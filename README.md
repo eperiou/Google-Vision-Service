efc1406e3511d424ee1359048eec753d054b3217
defields923 <defields923@gmail.com>
2017-03-14 17:05:50 -0500

    (feature) server makes takes and makes requests for vision api

---
+++ README.md
@@ -0,0 +1,14 @@
+# Breadcrumb
+
+## Google Vision API service
+
+This service accepts requests from client containing an image and returns a
+responses object back from the Google Vision API.
+
+To start:
+
+npm install
+
+Make .env and set variable GOOGLE_VISION to your API key with billing enabled.
+Acquire a GOOGLE_APPLICATION_CREDENTIALS file and place in root directory,
+make .env variable pointing to that filename in root.
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+require('dotenv').config();
+const GOOGLE_VISION = process.env.GOOGLE_VISION;
+const GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+const rp = require('request-promise');
+
+
+app.post('/picture/labels', (req, res) => {
+  const options = {
+    method: 'POST',
+    uri: `https://vision.googleapis.com/v1/images:annotate?key=${GOOGLE_VISION}`,
+    body: {
+        requests: req.body.requests,
+    },
+    json: true // Automatically stringifies the body to JSON
+  };
+
+  rp(options)
+    .then(function (parsedBody) {
+      // POST succeeded...
+      // response comes back in the form of responses[0].****Annotations, which is
+      // an array of objects with description and score
+      //  console.log(parsedBody.responses);
+      // console.log(parsedBody.responses[0].logoAnnotations);
+      // console.log(parsedBody.responses[0].labelAnnotations);
+      res.send(parsedBody.responses);
+    })
+    .catch(function (err) {
+      // POST failed...
+      throw new Error(err);
+    });
+});
+
+
+app.listen(PORT, function() {
+  console.log('HEY LISTEN', PORT);
+});
